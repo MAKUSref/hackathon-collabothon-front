8798e95705e309dd4e34f4904a75c9d8ca0449b3
MAKUSref <tomaszmakowski002731@gmail.com>
2023-10-21 23:33:22 +0200

    Add api endpoints and icons

--- src/redux/api/carbbynApi.ts
+++ src/redux/api/carbbynApi.ts
@@ -1,11 +1,34 @@
-import { baseApi } from './baseApi';
+import { baseApi } from "./baseApi";
 
-export const authApi = baseApi.injectEndpoints({
+export interface LinkedApp {
+  name: string;
+  logo: string;
+}
+
+export interface HistoryItem {
+  linkedApp: LinkedApp;
+  CC: number;
+  dateTime: string; //DD-MM-YYTHH-mm-ss
+}
+
+export const carbbynApi = baseApi.injectEndpoints({
   endpoints: (builder) => ({
     login: builder.mutation<{ message: string }, void>({
-      query: () => '/ping'
-    })
-  })
+      query: () => "/ping",
+    }),
+    getCC: builder.query<{ CC: number }, void>({
+      query: () => "/self/cc",
+    }),
+    getHistory: builder.query<HistoryItem[], void>({
+      query: () => "/self/history",
+    }),
+    getLinkedApps: builder.query<LinkedApp[], void>({
+      query: () => "/linkedApp",
+    }),
+    getLinkedApp: builder.query<LinkedApp[], string>({
+      query: (id) => `/linkedApp/${id}`,
+    }),
+  }),
 });
 
-export const { useLoginMutation } = authApi;
+export const { useLoginMutation } = carbbynApi;
